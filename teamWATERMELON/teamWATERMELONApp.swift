182c4f7dbbc44ee092c051aaef5f37f705e64414
11 <mobileappdev@bths.edu>
2026-04-23 14:27:08 -0400

    Initial Commit

---
+++ teamWATERMELON/teamWATERMELONApp.swift
@@ -0,0 +1,17 @@
+//
+//  teamWATERMELONApp.swift
+//  teamWATERMELON
+//
+//  Created by Student on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct teamWATERMELONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
