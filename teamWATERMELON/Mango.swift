047f8fdba810ebb9aebaf8a606cd8b85c8c53a56
11 <mobileappdev@bths.edu>
2026-04-23 14:42:23 -0400

    Added an image and added the file Mango with some text

---
+++ teamWATERMELON/Mango.swift
@@ -0,0 +1,21 @@
+//
+//  Mango.swift
+//  teamWATERMELON
+//
+//  Created by Student on 4/23/26.
+//
+
+import SwiftUI
+
+struct Mango: View {
+    var body: some View {
+        Image("MANGO")
+            .resizable()
+            .frame(width: 150, height: 150)
+        Text("IT'S A MANGO")
+    }
+}
+
+#Preview {
+    Mango()
+}
